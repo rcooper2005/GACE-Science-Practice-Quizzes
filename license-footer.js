085ba3ba1c2a8b86592c78e1fe60285636e89b9f
rcooper2005 <drpenwell@yahoo.com>
2026-09-21 14:56:20 -0400

    Add files via upload

--- license-footer.js
+++ license-footer.js
@@ -15,11 +15,11 @@
     '<p style="margin:0 0 .6rem;"><a href="' + LICENSE_URL + '" rel="license" target="_blank">' +
       '<img src="https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-nc-sa.svg" ' +
       'alt="Creative Commons BY-NC-SA 4.0 license" width="88" height="31" style="border:0;vertical-align:middle;"></a></p>' +
-    '<p style="margin:0 0 .5rem;"><em>GACE Science Practice Exams</em> &copy; ' + YEAR + ' by ' + AUTHOR +
+    '<p style="margin:0 0 .5rem;"><em>GACE Science of Reading Practice Exams</em> &copy; ' + YEAR + ' by ' + AUTHOR +
       ', Georgia Gwinnett College, is licensed under <a href="' + LICENSE_URL +
       '" rel="license" target="_blank" style="color:inherit;">CC BY-NC-SA 4.0</a>. ' +
       'You may share and adapt these materials for noncommercial use with attribution, under the same license.</p>' +
-    '<p style="margin:0;font-size:.78rem;">Independent study aids. Not affiliated with or endorsed by Pearson or the Georgia Professional Standards Commission. ' +
-      'GACE&reg; is a registered trademark of the Georgia Professional Standards Commission.</p>';
+    '<p style="margin:0;font-size:.78rem;">GACE&reg; is a registered trademark of the Georgia Professional Standards Commission. ' +
+      'These practice exams were developed independently and are not affiliated with or endorsed by the GaPSC or Pearson.</p>';
   document.body.appendChild(footer);
 })();
